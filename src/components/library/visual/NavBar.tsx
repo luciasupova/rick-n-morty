ba0d59581494fe4926ea6460a69222291326b669
luciasupova <luciasupovaa@gmail.com>
2023-02-23 12:06:50 +0100

    navbar

--- src/components/library/visual/NavBar.tsx
+++ src/components/library/visual/NavBar.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import './NavBar.css';
 
-interface HeaderProps {
+interface NavBarProps {
   title1: string;
   title2: string;
   title3: string;
 }
 
-export const Header = ({title1, title2, title3 }: HeaderProps) => (
+export const NavBar = ({title1, title2, title3 }: NavBarProps) => (
   <header>
     <div className="wrapper">
       <div>
         <img src="./src/assets/ricknmorty_logo.png" alt="" />
-        <h1>Acme</h1>
       </div>
       <div>
-        {user ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
+        <NavLink to="/page1">{title1}</NavLink>
+        <NavLink to="/page2">{title2}</NavLink>
+        <NavLink to="/page3">{title3}</NavLink>
       </div>
+      <div>Placeholder text</div>
     </div>
   </header>
 );
+
