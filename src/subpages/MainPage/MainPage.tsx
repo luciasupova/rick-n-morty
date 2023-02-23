33218e3984300fed07eaca0497a916069d3a1528
luciasupova <luciasupovaa@gmail.com>
2023-02-23 13:46:32 +0100

    main

--- src/subpages/MainPage/MainPage.tsx
+++ src/subpages/MainPage/MainPage.tsx
@@ -7,7 +7,7 @@ import Card from "../../components/library/visual/Card";
 export default function MainPage() {
     return (
         <section className="MainPage-section">
-            <img src="./src/assets/ricknmorty_biglogo.png" alt="" />
+            <img src="./src/assets/ricknmorty_biglogo.png" alt="biglogo" style={{ width: "600px", height: "200px" }}/>
             <Card title={"Rick"} subtitle={"awesome"}/>
             <Avatar />
             <Avatar isDead={true}/>
